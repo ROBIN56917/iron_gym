50bd029a8af41d737a45a63aa76d1c5ee59fb885
Robinson Hoyos <robinson56917@gmail.com>
2025-10-22 01:39:10 -0500

    Mi primer versión api iron_gym

---
+++ src/main/java/co/edu/umanizales/iron_gym/IronGymApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.iron_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IronGymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/umanizales/iron_gym/IronGymApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.iron_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
